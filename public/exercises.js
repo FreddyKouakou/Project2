657292f0ec9b629f1360b6ba4c7b59c997de3ff3
Freddy Kouakou <kou17001@byui.edu>
2019-03-22 23:32:51 +0000

    Week 11 work on exercises

--- public/exercises.js
+++ public/exercises.js
@@ -2,14 +2,41 @@ function exercises(questionNumberValue){
     //call web service to provide the necessary information from the database
     query = {questionNumber : questionNumberValue}
     $.get("exercisePath", query, (data)=>{
-        if(!data)console.log("Fail to provide question")
+         if(!data)console.log("Fail to provide question")
         else{
-            $("#questions").html(data.question);
-            $("#answera").html(data.answerA);
-            $("#answerb").html(data.answerB);
-            $("#answerc").html(data.answerC);
+            $("#questions").html(data[0].question);
+            $("#answera").html(`<input value="A" type="radio" name="btn">`+ data[0].answera);
+            $("#answerb").html(`<input value="B" type="radio" name="btn">`+ data[0].answerb);
+            $("#answerc").html(`<input value="C" type="radio" name="btn">`+ data[0].answerc);
+            $("#correctAnswers").val(data[0].correct_answer);
+            $("#questionNumbers").val(data[0].question_id);
         }
 
     });
-    
+
+        $("#submitAnswers").html(`<button onClick="submitAnswers()">Submit Answer</button>`);
+}
+/**
+ * Submit Answers
+ */
+function submitAnswers(){
+    var selectedAnswer = $(`[name="btn"]:checked`).val();
+    var theCorrectAnswer = $("#correctAnswers").val();
+    var questionNum = Number($("#questionNumbers").val()) +1;
+    console.log(questionNum);
+    if(selectedAnswer == theCorrectAnswer){
+       
+        alert("Well done!");
+        exercises(questionNum);
+    }else{
+        alert("Give it another try!");
+    }
+}
+
+/**
+ * Start Button
+ */
+function startButton(){
+    $("#startBtn").html("");
+    exercises(1);
 }
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.static("public")) //My html file
 * Function:app.get("/createUser")
 * Description: This block insert the user's information in the Database
 * The user information is provided the by the user through the 
-static file (html form) located in the public folder
+* static file (html form) located in the public folder
  ******************************************************************************/
 app.get("/createUser", (req, res) => {
     var firstName = req.query.first_name;
@@ -36,14 +36,24 @@ app.get("/createUser", (req, res) => {
     var insertIntodb = "INSERT INTO user_table(first_name, last_name, users_name, phone_number, pass_word) VALUES($1, $2, $3, $4, $5) " //This insert the user's data in the variable $1...5 in the query
     pool.query(insertIntodb, userInfo, (err, result) => {
         if (err) console.log("Didn't get the user info" + err);
-        else { res.send("User Account successfully created.") }
+        else { 
+            console.log("User Account successfully created.")
+            res.redirect("signin.html") 
+        }
+    
     });
+
 });
+/******************************************************************************
+ * Function: GET()
+ * Description: This function retrieve the questions and send it them to 
+ * the  browser.(Querying questions form the questions table)
+ ******************************************************************************/
 //Displays the json in the browser
 app.get("/getUser/:users_name", (req, res) => {
     var getUserName = req.params.users_name
     var user_name_on_try = [getUserName]
-    pool.query("SELECT *FROM user_table WHERE users_name = $1", user_name_on_try, (err, result) => {
+    pool.query("SELECT * FROM user_table WHERE users_name = $1", user_name_on_try, (err, result) => {
         if (err) console.log("Still given troubles" + err)
         else {
             var displayNameArray = result.rows
@@ -51,8 +61,11 @@ app.get("/getUser/:users_name", (req, res) => {
         }
     });
 });
-
-//Querying questions form the questions table
+/******************************************************************************
+ * Function: GET()
+ * Description: This function retrieve the questions and send it them to 
+ * the  browser.(Querying questions form the questions table)
+ ******************************************************************************/
 app.get("/getQuestions/:question_id", (req, res) => {
     var getQuestion = req.params.question_id
     var user_question = [getQuestion]
@@ -64,7 +77,11 @@ app.get("/getQuestions/:question_id", (req, res) => {
         }
     });
 });
-
+/******************************************************************************
+ * Function: POST()
+ * Description: This function verifies the user's sign in information
+ * password and username
+ ******************************************************************************/
 app.post("/signin", (req, res) => {
     var userSignIn = req.body.username;
     var userPassWord = req.body.password;
@@ -77,21 +94,22 @@ app.post("/signin", (req, res) => {
         console.log(row.users_name);
         if(err) console.log("Error getting the user name")
         if(row.pass_word == userPassWord){
-            res.redirect("homepage.html"); 
+            res.redirect("exercises.html"); 
     }else{
         res.redirect("signin.html");
     }
 });
 });
-/***
+/******************************************************************************
  * Create the web service for the question 
  * using get()
- */
+ ******************************************************************************/
 app.get("/exercisePath", (req, res)=>{
 var questionNumberSent = req.query.questionNumber;
 pool.query("SELECT * FROM questions WHERE question_id = $1", [questionNumberSent], (err, result)=>{
     if(err) console.log("Couldn't get questions")
     else{
+        console.log(result.rows);
         res.json(result.rows);
     }
 });
